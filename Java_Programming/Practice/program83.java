080eb420c8a13c8b1809dba42a0d594cca5fa938
SamruddhiKulkarni123 <kulkarnisamruddhi44@gmail.com>
2026-08-15 00:43:07 +0530

    Add files via upload

---
+++ Java_Programming/Practice/program83.java
@@ -0,0 +1,21 @@
+// Display the length of array and elements of array
+
+import java.util.*;
+
+class program83
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int Arr[] = {10,20,30,40,50};
+        
+        System.out.println(Arr.length);
+
+        System.out.println(Arr[0]);
+        System.out.println(Arr[1]);
+        System.out.println(Arr[2]);
+        System.out.println(Arr[3]);
+        System.out.println(Arr[4]);
+    }
+}
