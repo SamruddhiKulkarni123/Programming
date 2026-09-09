6ee485ffcee9f90ab5406d260a9a621b5bffc16d
SamruddhiKulkarni123 <kulkarnisamruddhi44@gmail.com>
2026-09-09 12:14:16 +0530

    Update program28.java

--- Java_Programming/CompetitiveProgramming/program28.java
+++ Java_Programming/CompetitiveProgramming/program28.java
@@ -22,7 +22,7 @@ class program28
         Scanner sobj = new Scanner(System.in);
 
         System.out.println("Enter number : ");
-        n = sobj.nextInt();;
+        n = sobj.nextInt();
 
         for(i = 1; i <= n; i++)
         { 
